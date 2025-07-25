41cf09894a0cfb03794be63eda806786984bd38f
Your Name <you@example.com>
2025-07-25 18:35:26 -0300

    new: Created class seller

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,13 +1,21 @@
 package application;
 
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.Locale;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Department department = new Department(1, "Books");
-		System.out.println(department);
-
+		Locale.setDefault(Locale.US);
+		Department obj= new Department(1, "Books");
+		
+		Seller seller = new Seller(23, "Bob", "bobbrown@gmail.com", new Date(), 3900.50, obj);
+		
+		System.out.println(seller);
 	}
 
 }
